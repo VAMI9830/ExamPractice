402c7f9ed745895764c5ecc37400d4bbcb88c6f7
VAMI9830 <11700224@student.pxl.be>
2020-01-16 15:31:11 +0100

    baking the cookie

--- Program.cs
+++ Program.cs
@@ -9,6 +9,8 @@ namespace ExamPractice
             Console.WriteLine("Hello World!");
             Console.WriteLine("Poggers");
             Cookie cookie = new Cookie();
+            Console.WriteLine("bake the cookie");
+            Console.log("Cookie is ready");
         }
     }
 }
