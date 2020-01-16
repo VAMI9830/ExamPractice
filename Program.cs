f7ba6a7f42ce8bdbbbea942c0b8afd9c3ec5f3e9
VAMI9830 <11700224@student.pxl.be>
2020-01-16 15:49:56 +0100

    ssss

--- Program.cs
+++ Program.cs
@@ -7,7 +7,7 @@ namespace ExamPractice
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            Console.WriteLine("Poggers");
+            Console.WriteLine("Poggersssssssss");
             Cookie cookie = new Cookie();
             Console.WriteLine("bake the cookie");
             Console.log("Cookie is ready");
